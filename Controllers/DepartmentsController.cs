5a3f44c45b0f5c60f79556b2795c14b6c001642f
Lucas César <lucascesarmengo@gmail.com>
2026-01-28 12:46:43 -0300

    Application created

---
+++ Controllers/DepartmentsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesApp.Models;
+
+namespace SalesApp.Controllers;
+
+public class DepartmentsController: Controller
+{
+    public IActionResult Index()
+    {
+        List<Department> list = new List<Department>();
+        list.Add(new Department{Id=1, Name="Electronics"});
+
+        return View();
+    }
+}
---
+++ Models/Department.cs
@@ -0,0 +1,7 @@
+﻿namespace SalesApp.Models;
+
+public class Department
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
